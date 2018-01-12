44579b161fdf5200dd86286b72b2c994f704b6cb
ZL_ZYD <910186681@qq.com>
2018-01-12 10:03:29 +0800

    monitor

--- ui/src/main/java/com/zyd/ui/service/SomeHystrixService.java
+++ ui/src/main/java/com/zyd/ui/service/SomeHystrixService.java
@@ -1,9 +1,10 @@
 package com.zyd.ui.service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class SomeHystrixService {
