fec1bf4147836e2c873e7cd78b8322a6393d6aa6
ZL_ZYD <910186681@qq.com>
2018-01-12 13:41:27 +0800

    success

--- ui/src/main/java/com/zyd/ui/UiApplication.java
+++ ui/src/main/java/com/zyd/ui/UiApplication.java
@@ -2,10 +2,13 @@ package com.zyd.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * EnableZuulProxy 开启网关代理
@@ -20,5 +23,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class UiApplication {
     public static void main(String[] args) {
         SpringApplication.run(UiApplication.class, args);
+    }
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+       return builder.build();
     }
 }
